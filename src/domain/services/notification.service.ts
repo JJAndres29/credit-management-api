21fd635b75f4249c1c6563294dae1df069e3c210
Andres Jimenez <jandres666.aj@gmail.com>
2026-04-18 11:25:17 -0500

    Pdf en whatsapp implementado

--- src/domain/services/notification.service.ts
+++ src/domain/services/notification.service.ts
@@ -1,4 +1,11 @@
 export interface NotificationService {
   sendWhatsApp(to: string, message: string): Promise<boolean>;
   sendTemplate(to: string, templateName: string, variables: string[], languageCode?: string): Promise<boolean>;
+  sendDocument(
+    to: string,
+    document: Buffer,
+    filename: string,
+    caption?: string,
+    mimeType?: string,
+  ): Promise<boolean>;
 }
